4feae607b2236ba77cffe0bae98b1d9d9ea2fd23
NielsenErik <eriknielsen98@gmail.com>
2023-04-29 11:53:23 +0200

    added some comments

--- Test/cocoLoad.py
+++ Test/cocoLoad.py
@@ -65,37 +65,3 @@ class RefCOCO_Split(RefCOCO):
     
     def __getitem__(self, idx):
         return super().__getitem__(idx)
-    
-
-#refer = REFER(data_root, dataset='refcocog', splitBy='umd') # dataset='refcoco', splitBy='unc'    
-    
-# data = RefCOCO(annotations_file = 'refcocog/annotations/refs(umd).p', img_dir = 'refcocog/images')
-# def get_data(batch_size, annotations_file, img_root):
-    # transform = list()
-    # # resize each PIL image to 256 x 256
-    # transform.append(T.Resize((256, 256)))
-    # # convert Numpy to Pytorch Tensor
-    # transform.append(T.ToPILImage())
-    # transform.append(T.ToTensor())
-    # transform.append(T.Normalize(mean=[0.485, 0.456, 0.406],
-    #                              std=[0.229, 0.224, 0.225]))
-    
-    
-    # transform = T.Compose(transform)
-    # refCOCO_data = RefCOCO(annotations_file = annotations_file, img_dir=img_root, transform=transform)
-    # num_samples = len(refCOCO_data)
-#     training_samples = int(num_samples * 0.8+1)
-#     test_samples = num_samples - training_samples
-#     training_data, test_data = torch.utils.data.random_split(
-#         refCOCO_data, [training_samples, test_samples])
-#     train_loader = torch.utils.data.DataLoader(
-#         training_data, batch_size=batch_size, shuffle=True)
-#     test_loader = torch.utils.data.DataLoader(
-#         test_data, batch_size=batch_size, shuffle=False)
-#     return train_loader, test_loader
-
-# loader,_ = get_data(1, 'refcocog/annotations/refs(umd).p', 'refcocog/images')
-# i=0
-# for x in loader:
-#     i = i+1
-#     print(i)
