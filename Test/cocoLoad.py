a544edcf6e8b7a41191072ad84189fe1b4ff69b2
NielsenErik <eriknielsen98@gmail.com>
2023-04-29 11:52:34 +0200

    added some comments

--- Test/cocoLoad.py
+++ Test/cocoLoad.py
@@ -10,6 +10,14 @@ from clip import clip
 from PIL import Image
 
 class RefCOCO(Dataset):
+    # This class is used to load the RefCOCO dataset, it is a subclass of Dataset.
+    # It loads the dataset and to preprocess the images and the labels.
+    # the requried parameters are:
+    # annotations_file: the path to the pickle file containing the labels (RefCOCO folder provide a pickle file called refs(umd).p)
+    # img_dir: the path to the folder containing the images (RefCOCO folder provide a subfolder called images)
+    # transform: the transformation to be applied to the images
+    # target_transform: the transformation to be applied to the labels
+
     def __init__(self, annotations_file, img_dir, transform=None, target_transform=None):
         x = pd.read_pickle(annotations_file)
         self.img_labels = pd.DataFrame(x)
@@ -38,6 +46,16 @@ class RefCOCO(Dataset):
         return image, label
     
 class RefCOCO_Split(RefCOCO):
+    # This class is used to load the RefCOCO dataset, it is a subclass of Dataset.
+    # It loads the dataset and to preprocess the images and the labels and then
+    # it splits the dataset in training and test set depending on the split_type parameter.
+    # the requried parameters are:
+    # annotations_file: the path to the pickle file containing the labels (RefCOCO folder provide a pickle file called refs(umd).p)
+    # img_dir: the path to the folder containing the images (RefCOCO folder provide a subfolder called images)
+    # split_type: the type of split to be used, it can be 'train' or 'test'
+    # transform: the transformation to be applied to the images
+    # target_transform: the transformation to be applied to the labels
+
     def __init__(self, annotations_file, img_dir, split_type = 'train', transform=None, target_transform=None):
         super().__init__(annotations_file, img_dir, transform, target_transform)
         self.img_labels = self.img_labels.loc[self.img_labels['split'] == split_type]
