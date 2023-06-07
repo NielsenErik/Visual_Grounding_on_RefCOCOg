350205ef8b0b47dd7e333b56bc539f8c63a6a8c3
JvJ00 <manuel.righele@studenti.unitn.it>
2023-06-07 20:52:22 +0200

    merged

--- Test/triesErik.py
+++ Test/triesErik.py
@@ -4,31 +4,16 @@ import torch
 from PIL import Image
 import cv2
 import torchvision.transforms as T
-from cocoLoad import RefCOCO, RefCOCO_Split #Importing REfCOCO class from cocoLoad.py
+from cocoLoad import RefCOCO #Importing REfCOCO class from cocoLoad.py
 from transformers import CLIPProcessor, CLIPModel
 import clip
 from printCalls import error, warning, debugging, info 
 from customClip import CustomClip
-from model_utilis import save_model, load_personal_model
+from model_utilis import save_model, load_model, TensorBoard
+import final_program
+import math
+from torch.utils.tensorboard import SummaryWriter
 
-def random_get_text(all_texts):
-    small_list = []
-    for i in range(1000):
-        small_list.append(all_texts[np.random.randint(0,len(all_texts))])
-    return small_list
-
-def get_all_texts(annotations_file, smallTest=False):
-    x = pd.read_pickle(annotations_file)
-    img_texts = pd.DataFrame(x)
-    all_texts = []
-    for x in img_texts.iloc():
-        if x['split']=='test' and len(x['sentences'][0]['raw'])>0:
-            all_texts.append(x['sentences'][0]['raw'])
-    info("Number of texts: " + str(len(all_texts)))
-    if smallTest:
-        all_texts = random_get_text(all_texts)
-    info("Number of random sample: " + str(len(all_texts)))
-    return all_texts
 
 def random_get_text(all_texts):
     small_list = []
@@ -43,10 +28,10 @@ def get_all_texts(annotations_file, smallTest=True):
     for x in img_texts.iloc():
         if x['split']=='test' and len(x['sentences'][0]['raw'])>0:
             all_texts.append(x['sentences'][0]['raw'])
-    info("Number of texts: " + str(len(all_texts)))
+    #info("Number of texts: " + str(len(all_texts)))
     if smallTest:
         all_texts = random_get_text(all_texts)
-    info("Number of random sample: " + str(len(all_texts)))
+    #info("Number of random sample: " + str(len(all_texts)))
     return all_texts
 
 def putTextBg(img, text, org, font, size, fg_color, thickness, linetype, bg_color):
@@ -86,15 +71,16 @@ def get_img_transform():
     transform = T.Compose(transform)
     return transform
 
-def get_data(batch_size, annotations_file, img_root, model, preprocess = None, device = get_device(), sample_size = 5023):
+def get_data(batch_size, annotations_file, img_root, model, preprocess = None, device = get_device(), sample_size_train = 42226, sample_size_test = 5023, sample_size_val = 2573):
 
     transform = get_img_transform()
-      
-    training_data = RefCOCO_Split(annotations_file = annotations_file, img_dir=img_root, model = model, preprocess = preprocess, split_type='train', transform=transform, device=device, sample_size=sample_size, batch_size=batch_size)
-    if sample_size > 5023:
-        sample_size = 5023 
-    test_data = RefCOCO_Split(annotations_file = annotations_file, img_dir=img_root, model = model, preprocess = preprocess, split_type='test', transform=transform, device=device, sample_size=sample_size, batch_size=batch_size)
-    eval_data = RefCOCO_Split(annotations_file = annotations_file, img_dir=img_root, model = model, preprocess = preprocess, split_type='val', transform=transform, device=device, sample_size=int(sample_size*0.01), batch_size=batch_size)
+    
+    sample_size_train = sample_size_train if sample_size_train <= 42226 else 42226
+    sample_size_test = sample_size_test if sample_size_test <= 5023 else 5023
+    sample_size_val = sample_size_val if sample_size_val <= 2573 else 2573
+    training_data = RefCOCO(annotations_file = annotations_file, img_dir=img_root, model = model, preprocess = preprocess, split_type='train', transform=transform, device=device, sample_size=sample_size_train, batch_size=batch_size, augment_data=True)
+    test_data = RefCOCO(annotations_file = annotations_file, img_dir=img_root, model = model, preprocess = preprocess, split_type='test', transform=transform, device=device, sample_size=sample_size_test, batch_size=batch_size)
+    eval_data = RefCOCO(annotations_file = annotations_file, img_dir=img_root, model = model, preprocess = preprocess, split_type='val', transform=transform, device=device, sample_size=sample_size_val, batch_size=batch_size)
 
     num_training_samples = len(training_data)
     info("Number of training samples:" + str(num_training_samples))
@@ -103,8 +89,9 @@ def get_data(batch_size, annotations_file, img_root, model, preprocess = None, d
     num_eval_samples = len(eval_data)
     info("Number of eval samples:" + str(num_eval_samples))
     train_loader = torch.utils.data.DataLoader(training_data, batch_size=batch_size, shuffle=True)
-    test_loader = torch.utils.data.DataLoader(test_data, batch_size=batch_size, shuffle=False)
-    return train_loader, test_loader, eval_data
+    test_loader = torch.utils.data.DataLoader(test_data, batch_size=1, shuffle=False)
+    eval_loader = torch.utils.data.DataLoader(eval_data, batch_size=1, shuffle=False)
+    return train_loader, test_loader, eval_loader
 
 def empty_token(model, device):
     empty_desc = clip.tokenize("").to(device)
@@ -146,15 +133,14 @@ def training_step(model, train_dataloader,  optimizer, cost_function=get_cost_fu
         #         p.grad.data = p.grad.data.float() 
         
         clip.model.convert_weights(model)
-    return cumulative_loss / samples, cumulative_accuracy / samples * 100
+    return cumulative_loss / samples, cumulative_accuracy / samples
 
 def test_step(model, test_loader, cost_function, device=get_device()):
     samples = 0.0
     cumulative_loss = 0.0
     cumulative_accuracy = 0.0
     model.eval() 
-    debugging("Testing")
-  # disable gradient computation (we are only testing, we do not want our model to be modified in this step!)
+    # disable gradient computation (we are only testing, we do not want our model to be modified in this step!)
     with torch.no_grad():
         # iterate over the test set
         for (images, texts) in test_loader:
@@ -172,7 +158,7 @@ def test_step(model, test_loader, cost_function, device=get_device()):
             _, predicted = logits_per_image.max(dim=1)    
             cumulative_accuracy += predicted.eq(ground_truth).sum().item()
         
-    return cumulative_loss / samples, cumulative_accuracy / samples * 100
+    return cumulative_loss / samples, cumulative_accuracy / samples
 
 def get_texts(data, device = get_device()):
     text = []
@@ -182,7 +168,7 @@ def get_texts(data, device = get_device()):
             clip_targets = clip.tokenize(text).squeeze().to(device)
     return text, clip_targets
 
-def eval_step(clip_model, clip_processor, data, coco_desc, device = get_device(), transform = get_img_transform()):   
+def eval_step(clip_model, clip_processor, data, coco_desc, device = get_device()):   
     clip_threshold = 0.0005
     clip_targets = clip.tokenize(coco_desc).squeeze().to(device)
     with torch.no_grad(): #important to mantain memory free  
@@ -215,6 +201,13 @@ def final_step(clip_model):
     cv2.imshow("Result", img)
     cv2.waitKey(0)
 
+def update_parameters(learning_rate, weight_decay, momentum, alpha):
+    learning_rate = learning_rate * alpha
+    weight_decay = weight_decay * alpha
+    momentum = momentum * alpha
+    alpha = alpha/(alpha+0.001)
+    return learning_rate, weight_decay, momentum, alpha
+
 def main():
     batch_size = 16 #must be 16 due to lenght of clip_targets
     device = 'cuda:0'
@@ -223,34 +216,63 @@ def main():
     weight_decay = 0.000001
     momentum = 0.9
     epochs = 20
+    e = math.exp(1)
+    alpha = 1
+    visualization_name='RefCOCOg'
     annotations_file = 'refcocog/annotations/refs(umd).p'
     root_imgs = 'refcocog/images'
     all_texts = get_all_texts(annotations_file)
     #yolo_model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True, _verbose=False)
-    clip_model = CustomClip(device=get_device(), batch_size=batch_size, norm=True)
+    clip_model = CustomClip(device=get_device(), batch_size=batch_size, norm=False, bias=True)
     _ , clip_processor = clip_model.__get_model__()
     #clip_model, clip_processor = clip.load('RN50', device, jit=False)
-    optimizer = get_optimizer(clip_model, learning_rate, weight_decay, momentum)
+    
+    train_loader, test_loader, val_loader = get_data(batch_size, annotations_file=annotations_file, img_root=root_imgs, model=clip_model, preprocess=clip_processor, sample_size_train=1000, sample_size_test=100, sample_size_val=50)
 
-    train_loader, test_loader, test_data = get_data(batch_size, annotations_file=annotations_file, img_root=root_imgs, model=clip_model, preprocess=clip_processor, sample_size=2096)
+    #eval_step(yolo_model, clip_model, clip_processor, val_loader)
+    #desc, tmp = get_texts(val_loader)
+    
+    tb = TensorBoard("run")
+    
+    info("BEFORE TRAINING...")
+    loss, accuracy = test_step(clip_model, train_loader, cost_function)
+    info("Train - LOSS: {:.4} ACCURACY: {:2.1%}%".format(loss, accuracy))
+    tb.log_values(-1, loss, accuracy, "Train")
+    loss, accuracy = test_step(clip_model, val_loader, cost_function)
+    info("Validation - LOSS: {:.4} ACCURACY: {:2.1%}%".format(loss, accuracy))
+    tb.log_values(-1, loss, accuracy, "Validation")
+    loss, accuracy = test_step(clip_model, test_loader, cost_function)
+    info("Test - LOSS: {:.4} ACCURACY: {:2.1%}%".format(loss, accuracy))
+    tb.log_values(-1, loss, accuracy, "Test")
 
-    #eval_step(yolo_model, clip_model, clip_processor, test_data)
-    #desc, tmp = get_texts(test_data)
-    info("Init training...")
-
+    info("INIT TRAINING...")
     for ep in range(epochs):
         info("EPOCH "+str(ep)+":")
         loss, accuracy = training_step(clip_model, train_loader, optimizer, cost_function)
-        info("LOSS: "+str(loss)+" ACCURACY: "+str(accuracy)+"%")
-        #clip.model.convert_weights(clip_model)
-    info("TESTING:")
-    
-    loss, accuracy =test_step(clip_model, test_loader, cost_function)
-    save_model(clip_model, epochs, optimizer, loss, "Personal_Model")
-    info("LOSS: "+str(loss)+" ACCURACY: "+str(accuracy)+"%")  
+        info("Train - LOSS: {:.4} ACCURACY: {:2.1%}%".format(loss, accuracy))
+        tb.log_values(ep, loss, accuracy, "Train")
+        loss, accuracy = test_step(clip_model, val_loader, cost_function)
+        info("Validation - LOSS: {:.4} ACCURACY: {:2.1%}%".format(loss, accuracy))
+        tb.log_values(ep, loss, accuracy, "Validation")
 
-    model, optimizer, epoch, loss = load_personal_model(clip_model, optimizer, "Personal_Model")
-    eval_step(model, clip_processor, test_data, all_texts, device=device, transform=get_img_transform())
-    final_step(model)
+    info("AFTER TRAINING...")
+    loss, accuracy = test_step(clip_model, train_loader, cost_function)
+    info("Train - LOSS: {:.4} ACCURACY: {:2.1%}%".format(loss, accuracy))
+    tb.log_values(epochs, loss, accuracy, "Train")
+    loss, accuracy = test_step(clip_model, val_loader, cost_function)
+    info("Validation - LOSS: {:.4} ACCURACY: {:2.1%}%".format(loss, accuracy))
+    tb.log_values(epochs, loss, accuracy, "Validation")
+    loss, accuracy = test_step(clip_model, test_loader, cost_function)
+    info("Test - LOSS: {:.4} ACCURACY: {:2.1%}%".format(loss, accuracy))
+    tb.log_values(epochs, loss, accuracy, "Test")
+    tb.close()
+
+    save_model(clip_model, epochs, optimizer, loss, "Personal_Model")
+
+    info("EVALUATION...")
+    model, optimizer, epoch, loss = load_model(clip_model, optimizer, "Personal_Model")
+    #eval_step(model, clip_processor, val_loader, all_texts, device=device)
+    final_program.final_program(model)
+
 ##########################################################################################
 main()
