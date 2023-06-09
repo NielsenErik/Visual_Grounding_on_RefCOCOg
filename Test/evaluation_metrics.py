288f44ccdb5f5c81d87beaf31352b944d1a2b5d6
JvJ00 <manuel.righele@studenti.unitn.it>
2023-06-09 16:55:04 +0200

    in progress

--- Test/evaluation_metrics.py
+++ Test/evaluation_metrics.py
@@ -1,12 +1,22 @@
 import torch
-import clip
-import numpy as np
-import pandas as pd
-from PIL import Image
+import random
+import cv2
 from torchmetrics.classification import MulticlassRecall
 from torchvision.ops import box_iou
+from printCalls import info
+from customClip import CustomClip
+from cocoLoad import RefCOCO
 
-def recall(preds, target, num_labels, device = 'cuda'):
+get_device_first_call=True
+def get_device():
+    global get_device_first_call
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    if get_device_first_call:
+        info("The current device is " + device)
+        get_device_first_call=False
+    return device
+
+def recall(preds, target, num_labels, device = get_device()):
     # Recall is the fraction of relevant instances that have been retrieved over the total amount of relevant instances.
     # In the case of multiclass classification, the recall is the average of the recall of each class.
     # In the case of multilabel classification, the recall is the average of the recall of each label.
@@ -29,7 +39,7 @@ def intersection_over_union(preds, target):
     value = box_iou(preds, target)
     return value.item()
 
-def semantic_similarity(custom_model, imgs, texts):
+def cosine_similarity(custom_model, imgs, texts):
     # Semantic similarity is a metric defined over a set of documents or terms,
     # where the idea of distance between them is based on the likeness of their meaning or semantic content
     # Parameters:
@@ -45,6 +55,52 @@ def semantic_similarity(custom_model, imgs, texts):
     similarity = cos_sim(enc_imgs, enc_texts)
     return similarity
 
+def eval_step(model, eval_loader, device = get_device()):
+    samples = 0.0
+    comulative_recall = 0.0
+    cumulative_sim = 0.0
+    model.eval() 
+    # disable gradient computation (we are only testing, we do not want our model to be modified in this step!)
+    with torch.no_grad():
+        # iterate over the set
+        for (images, texts) in eval_loader:
+            images = images.to(device)
+            texts = texts.squeeze(1).to(device)
+            logits_per_image, logits_per_texts = model(images, texts)
+            ground_truth = torch.arange(len(images),dtype=torch.long,device=device)
+            samples += images.shape[0]  
+            n_labels = logits_per_texts.shape[1]
+            _, predicted = logits_per_image.max(dim=1)
+            comulative_recall += recall(predicted, ground_truth, n_labels, device)
+            cos_sim = cosine_similarity(model, images, texts)
+            cumulative_sim += torch.sum(cos_sim).item()
 
-if __name__ == "__main__":
-    pass
+    return comulative_recall / samples, cumulative_sim / samples
+
+
+clip_model = CustomClip(device=get_device())
+_, preprocess = clip_model.__get_model__()
+test_data = RefCOCO(annotations_file = 'refcocog/annotations/refs(umd).p', img_dir='refcocog/images', preprocess = preprocess, split_type='test', device=get_device(), sample_size=5000)
+test_loader = torch.utils.data.DataLoader(test_data, batch_size=16, shuffle=False)
+
+# Evaluate recall (grounding accuracy metric) and cosine similarity (semantic similarity metric)
+rec, sim = eval_step(clip_model, test_loader)
+info("RECALL: {:2.1%} SIMILARITY: {:2.1%}".format(rec, sim))
+
+
+# Evaluate IoU (localization accuracy metric)
+IoU = 0
+for i in range(10):
+    _, img = test_data.__getimg__(random.randint(0, test_data.__len__()))
+    cvimg = cv2.imread(img)
+    cv2.imshow("Input",cvimg)
+    cv2.waitKey(0)
+    info("Insert sentence:")
+    text=input()
+    item, _ = clip_model.__get_boxes__(img, text)
+    if item is not None:
+        cv2.rectangle(cvimg, (item["xmin"], item["ymin"]), (item["xmax"], item["ymax"]), (0,127,0), 3)
+        cv2.imshow("Output",cvimg)
+        cv2.waitKey(0)
+
+info("IoU: {:2.1%}".format(IoU))
